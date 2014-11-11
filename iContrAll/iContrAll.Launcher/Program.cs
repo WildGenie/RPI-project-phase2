2e9f5e94960a9674e6f616493c5c340ef96b9d13
bandidas <andrasszabo@outlook.com>
2014-11-11 18:18:13 +0100

    "Launcher" projekt az újraindításokhoz

---
+++ iContrAll/iContrAll.Launcher/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace iContrAll.Launcher
+{
+    class Program
+    {
+        static bool debug = false;
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0 && args[0] == "d")
+            {
+                debug = true;
+            }
+            else
+                debug = false;
+	        Launch();
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void Launch()
+        {
+            InitGPIOPins();
+
+            Process process = new Process();
+            process.StartInfo.FileName = "mono";
+            if (debug)
+                process.StartInfo.Arguments = "/home/pi/iContrAll/bin/Debug/iContrAll.TcpServer.exe 79.121.25.151 1123 alpha.icontrall.hu /home/pi/iContrAll/ca/server.p12 allcontri";
+            else
+                process.StartInfo.Arguments = "/home/pi/iContrAll/bin/iContrAll.TcpServer.exe 79.121.25.151 1123 alpha.icontrall.hu /home/pi/iContrAll/ca/server.p12 allcontri";
+            process.EnableRaisingEvents = true;
+            process.Exited += LaunchIfCrashed;
+
+            process.Start();
+        }
+
+        private static void LaunchIfCrashed(object o, EventArgs e)
+        {
+            InitGPIOPins();
+
+            Process process = (Process)o;
+            Console.WriteLine("Process crashed: {0}", process.StartInfo.FileName);
+            Console.WriteLine(process.ExitCode);
+            if (process.ExitCode != 0)
+            {
+                Launch();
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private static void InitGPIOPins()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "sudo";
+            
+            process.StartInfo.Arguments = "gpio mode 0 out";
+            process.Start();
+            Thread.Sleep(100);
+
+            process.StartInfo.Arguments = "gpio mode 7 down";
+            process.Start();
+            Thread.Sleep(100);
+
+            process.StartInfo.Arguments = "gpio mode 7 out";
+            process.Start();
+            Thread.Sleep(100);
+            
+	     process.StartInfo.Arguments = "gpio mode 2 in";
+            process.Start();
+
+            process.StartInfo.Arguments = "gpio mode 2 up";
+            process.Start();
+        }
+    }
+}
